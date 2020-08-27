96412faad6915d8a070074e8e10f00749675db94
Tom Casper <tmcasper@gmail.com>
2020-08-26 23:54:07 -0400

    Correcting issue with straights

--- src/main/scala/com/tcasper/TexasHoldem.scala
+++ src/main/scala/com/tcasper/TexasHoldem.scala
@@ -1,5 +1,11 @@
 package com.tcasper
 
+case class Card(value: (Int, Char, Char))
+
+case class Hand(hand: (Card, Card)) {
+  def displayHand() = s"${hand._1.value._2}${hand._1.value._3}${hand._2.value._2}${hand._2.value._3}"
+}
+
 object TexasHoldem extends App {
 
   final val cardValueMap = Map(
@@ -18,60 +24,49 @@ object TexasHoldem extends App {
     'K' -> 13)
 
   final val StraightFlush = 9
-  final val FourOfAKind = 8
-  final val FullHouse = 7
-  final val Flush = 6
-  final val Straight = 5
-  final val ThreeOfAKind = 4
-  final val TwoPair = 3
-  final val Pair = 2
-  final val HighCard = 1
+  final val FourOfAKind   = 8
+  final val FullHouse     = 7
+  final val Flush         = 6
+  final val Straight      = 5
+  final val ThreeOfAKind  = 4
+  final val TwoPair       = 3
+  final val Pair          = 2
+  final val HighCard      = 1
 
-  println("[Main with Card] --> Enter the given hand: ")
+  println("Enter the given hand: ")
   val cardString = scala.io.StdIn.readLine()
-
-  println("Unprocessed Card String ==> " + cardString)
 
   val cardArray = cardString.split(" ")
   val boardCards = cardArray(0)
 
-  println()
-  println("boardcards = " + boardCards)
-
-  println()
   val handsStringArray = cardArray.tail
-  print("hands = ")
-  handsStringArray.map(x => print(x + " "))
-  println()
-
   val boardCardsArray = processBoardCards(boardCards)
-  print("boardcards array = ")
-  boardCardsArray.map(x => print(" | " + x + " | "))
-  println()
   val handsArray = processHandCards(handsStringArray)
-  print("hands array = ")
-  handsArray.map(x => print(" | " + x + " | "))
 
   val handsMap =
     for (hand <- handsArray) yield process(boardCardsArray, hand)
 
-  print("Final HandsMap = ")
-  handsMap.sortBy(_._2).map(p => println(p._1.displayHand() + " Value = " + p._2))
+
+  handsMap.sortBy(_._2).map(p => println(p._1.displayHand() + " rank = " + p._2))
+  val sortedHandsMap = handsMap.sortBy(_._2)
+  for (i <- sortedHandsMap.indices) {
+    if (i != 0 && sortedHandsMap(i - 1)._2 == sortedHandsMap(i)._2) {
+      print("=" + sortedHandsMap(i)._1.displayHand())
+    } else {
+      print(" " + sortedHandsMap(i)._1.displayHand())
+    }
+  }
+
 
   def process(boardCards: Array[Card], hand: Hand): (Hand, Int) = {
 
     // Combine the hand with the board cards first
     val combined = combineBoardCardsWithHand(boardCards, hand)
-
     val sortedCards = sortCombinedCards(combined.toList)
-    println("sorted cards = " + sortedCards)
 
     //Cards will be grouped by value in descending order size groups
     val groupedCards = splitCardsByGroup(sortedCards)
-    println("grouped cards = " + groupedCards)
-
     val handValue = determineHandValue(groupedCards, hand)
-
     handValue
   }
 
@@ -79,7 +74,6 @@ object TexasHoldem extends App {
     val ungrouped = groupedCards.flatten.sortBy(_.value._1)
     groupedCards.length match {
       case 7 =>
-        println("Size 7")
         // 7 distinct card values
         if (isStraight(ungrouped) && isFlush(ungrouped)) {
           (originalHand, StraightFlush)
@@ -90,9 +84,9 @@ object TexasHoldem extends App {
         } else (originalHand, HighCard)
 
       case 6 =>
-        println("Size 6")
         // Pair, 5 singles
         if (isStraight(ungrouped) && isFlush(ungrouped)) {
+          println("straight flush")
           (originalHand, StraightFlush)
         } else if (isFlush(ungrouped)) {
           (originalHand, Flush)
@@ -101,21 +95,19 @@ object TexasHoldem extends App {
         } else (originalHand, Pair)
 
       case 5 =>
-        println("Size 5")
-        // 3 of a kind, 4 singles
         if (isFlush(ungrouped)) {
           (originalHand, Flush)
         }
+        // 3 of a kind, 4 singles
         else if (groupedCards.head.length == 3) {
           (originalHand, ThreeOfAKind)
 
-          // pair, pair, 3 singes
+          // pair, pair, 3 singles
         } else {
           (originalHand, TwoPair)
         }
 
       case 4 =>
-        println("Size 4")
         if (isFlush(ungrouped)) {
           (originalHand, Flush)
         }
@@ -135,7 +127,6 @@ object TexasHoldem extends App {
         }
 
       case 3 =>
-        println("Size 3")
         if (isFlush(ungrouped)) {
           (originalHand, Flush)
         }
@@ -151,7 +142,6 @@ object TexasHoldem extends App {
         }
 
       case 2 =>
-        println("Size 2")
         if (isFlush(ungrouped)) {
           (originalHand, Flush)
         }
@@ -169,7 +159,6 @@ object TexasHoldem extends App {
   }
 
   def sortCombinedCards(cards: List[Card]): List[Card] = {
-    println("List from combined cards = " + cards)
     val sortedList = cards.sortBy(_.value._1)
     sortedList
   }
@@ -181,14 +170,10 @@ object TexasHoldem extends App {
   }
 
   def isFlush(cards: List[Card]): Boolean = {
-    println("#### Checking for flush...")
     val grouped = cards.groupBy(_.value._3)
-    println(s"#### Grouped = $grouped")
     val groupedListsOfSuits = grouped.values
-    println(s"#### Grouped list of suits = $groupedListsOfSuits")
     for (g <- groupedListsOfSuits) {
       if (g.length >= 5) {
-        println(s"#### Found a group of 5 suits, returning true")
         return true
       }
     }
@@ -196,37 +181,26 @@ object TexasHoldem extends App {
   }
 
   def isStraight(cards: List[Card]): Boolean = {
-    println("#### Checking for a straight...")
     if (containsAnAce(cards)) {
       isStraightWithAce(cards)
     } else {
-      println("#### No ACES found, checking deck for straight...")
       for (i <- 0 until 4) {
-        println(s"iteration $i")
-        if ((cards(i + 1).value._1 != cards(i).value._1 + 1) && (i > 2)) {
-          println("Returning false")
-          println(s"cards(i + 1).value._1 != cards(i).value._1 + 1 = ${cards(i + 1).value._1 != cards(i).value._1 + 1}")
+        if ((cards(i + 1).value._1 != cards(i).value._1 + 1) && (i > 1)) {
           return false
         }
       }
-      println("#### Straight found, returning true")
       true
     }
   }
 
   def isStraightWithAce(cards: List[Card]): Boolean = {
-    println("#### Contains an ACE...")
     val bottomStraight = cards(1).value._1 == 2 && cards(2).value._1 == 3 && cards(3).value._1 == 4 && cards(4).value._1 == 5
-    println("#### Bottom straight = " + bottomStraight)
     val topStraight = cards(3).value._1 == 10 && cards(4).value._1 == 11 && cards(5).value._1 == 12 && cards(6).value._1 == 13
-    println("#### Top straight = " + topStraight)
     if (bottomStraight || topStraight) {
-      println("Bottom or top straight found, returning true")
       true
     } else {
-      println("#### No bottom or top straight found, checking middle...")
       for (i <- 0 until 4) {
-        if ((cards(i + 1).value._1 != cards(i).value._1 + 1) && (i > 2)) {
+        if ((cards(i + 1).value._1 != cards(i).value._1 + 1) && (i > 1)) {
           return false
         }
       }
@@ -257,14 +231,10 @@ object TexasHoldem extends App {
     val handsArray =
       for (
         str <- handsStringArray
-      ) yield Hand(Card(cardValueMap(str.charAt(0)), str.charAt(0).toUpper, str.charAt(1)), Card(cardValueMap(str.charAt(2)), str.charAt(2).toUpper, str.charAt(3)))
+      ) yield Hand(Card(cardValueMap(str.charAt(0)), str.charAt(0).toUpper, str.charAt(1)),
+        Card(cardValueMap(str.charAt(2)), str.charAt(2).toUpper, str.charAt(3)))
     handsArray
   }
-}
-
-case class Card(value: (Int, Char, Char))
-case class Hand(hand: (Card, Card)) {
-  def displayHand() = s"${hand._1.value._2}${hand._1.value._3}${hand._2.value._2}${hand._2.value._3}"
 }
 
 
